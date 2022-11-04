aa08e0739afb6404d03a173fbcc46344d516c44c
ssuyun.chen <ssuyun.chen@udngroup.com>
2022-11-04 09:20:18 +0800

    fix: 修正 api

--- src/service/api.js
+++ src/service/api.js
@@ -7,7 +7,7 @@ const baseRequest = axios.create({
 const userRequest = axios.create({
   baseURL: `${import.meta.env.VITE_APP_API}`,
   headers: {
-    Authorization: `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYzNjBjNmVmOTk2Y2Y3ZjUxNWI0NjRiYyIsImlhdCI6MTY2NzQ1ODU3OSwiZXhwIjoxNjY4MDYzMzc5fQ.3eCjq8LSuxqzWXsK38EZwzGni-WUS13pXU8uGuq5ing`
+    Authorization: `Bearer`
   }
 });
 
